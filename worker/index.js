d6f7ebdf07d98e4bbf13317cef10749d00180938
unknown <pranavkolhatkar312@gmail.com>
2021-08-08 17:18:58 +0530

    initial commit

---
+++ worker/index.js
@@ -0,0 +1,24 @@
+const keys = require('./keys')
+const redis = require('redis')
+
+const redisClient = redis.createClient({
+    host: redis.redisHost,
+    port:keys.redisPort,
+    retry_strategy: () => 1000
+})
+
+const sub = redisClient.duplicate()
+
+function fib(idx){
+    if(index < 2){
+        return 1;
+    }
+    return fib(idx-1) + fib(idx -2);
+}
+
+
+sub.on('message',(channel,message) => {
+redisClient.hset('values',message,fib(parseInt(message)))
+})
+
+sub.subscribe('insert')
