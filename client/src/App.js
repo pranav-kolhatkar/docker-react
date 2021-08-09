364ca3d6c82c3d62416e69c523664f731e274b96
unknown <pranavkolhatkar312@gmail.com>
2021-08-09 14:21:00 +0530

    client folder

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+import {BrowserRouter as Router,Route,Link } from 'react-router-dom'
+
+import OtherPage from './OtherPage'
+
+import Fib from './Fib'
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">Home</Link>
+        <Link to="otherPage">Other Page</Link>
+      </header>
+        <div> 
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherName" component={OtherPage} />
+        </div>
+     
+    </div>
+    </Router>
+  );
+}
+
+export default App;
